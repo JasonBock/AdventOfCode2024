551c1123c411de5e46083a171c664de65c046437
Jason Bock <jason.r.bock@outlook.com>
2024-12-22 16:07:45 -0600

    NUnit with the Testing.Platform bits

---
+++ src/AdventOfCode2024.Tests/SolutionDay1Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2024.Day1.Tests;
+
+public static class SolutionDay1Tests
+{
+	[Test]
+	public static void Add()
+	{
+		var x = 3;
+		var y = 4;
+		Assert.That(x + y, Is.EqualTo(7));
+	}
+}
